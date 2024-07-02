31b0ab89923412b4292ca2a297df2473b112c0ee
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2024-07-02 22:18:01 +0700

    update

---
+++ public/build/assets/IconWeight-Cb4JoSNa.js
@@ -0,0 +1,6 @@
+import{d as a}from"./AppLayout-BBXMtu6L.js";/**
+ * @license @tabler/icons-react v3.4.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var t=a("outline","weight","IconWeight",[["path",{d:"M12 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0",key:"svg-0"}],["path",{d:"M6.835 9h10.33a1 1 0 0 1 .984 .821l1.637 9a1 1 0 0 1 -.984 1.179h-13.604a1 1 0 0 1 -.984 -1.179l1.637 -9a1 1 0 0 1 .984 -.821z",key:"svg-1"}]]);export{t as I};
